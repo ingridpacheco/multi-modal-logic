6a85595983529e52538fa5805b619a87a4916473
ingridpacheco <iq.pacheco23@gmail.com>
2019-07-08 14:21:59 -0300

    Updates

---
+++ precache-manifest.23e157570112131c54b64eee1a442cff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "eedf613a18fe994087e7",
+    "url": "/multi-modal-logic/static/css/main.af2de749.chunk.css"
+  },
+  {
+    "revision": "eedf613a18fe994087e7",
+    "url": "/multi-modal-logic/static/js/main.15154b84.chunk.js"
+  },
+  {
+    "revision": "8bd4f39c74e8aaa1c473",
+    "url": "/multi-modal-logic/static/js/runtime~main.2adb4295.js"
+  },
+  {
+    "revision": "086019f7b75c1b91ae35",
+    "url": "/multi-modal-logic/static/js/2.5cbc308e.chunk.js"
+  },
+  {
+    "revision": "be70d7dd4747afad93cef1636ef707c0",
+    "url": "/multi-modal-logic/static/media/bg-index.be70d7dd.jpg"
+  },
+  {
+    "revision": "f94adfa71bf30704c5024f3f041e1393",
+    "url": "/multi-modal-logic/static/media/graph.f94adfa7.png"
+  },
+  {
+    "revision": "7cbec191d53c3bbe9997312332464b6f",
+    "url": "/multi-modal-logic/index.html"
+  }
+];
